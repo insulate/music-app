2c2f4eab88430841da922598d037aa4a80cf7147
biiwz <biiwzmethom@gmail.com>
2022-02-10 18:44:19 +0700

    fix css taiwind

--- src/App.vue
+++ src/App.vue
@@ -408,18 +408,7 @@
       </div>
       <!-- Current Position -->
       <div
-        class="
-          float-left
-          w-7
-          h-7
-          leading-3
-          text-gray-400
-          mt-0
-          text-lg
-          w-14
-          ml-5
-          mt-1
-        "
+        class="float-left h-7 leading-3 text-gray-400 text-lg w-14 ml-5 mt-1"
       >
         <span class="player-currenttime">00:00</span>
       </div>
@@ -475,18 +464,7 @@
       </div>
       <!-- Duration -->
       <div
-        class="
-          float-left
-          w-7
-          h-7
-          leading-3
-          text-gray-400
-          mt-0
-          text-lg
-          w-14
-          ml-8
-          mt-1
-        "
+        class="float-left h-7 leading-3 text-gray-400 text-lg w-14 ml-8 mt-1"
       >
         <span class="player-duration">03:06</span>
       </div>
