c74c0b70306138b15068b299341dfb510b5f7b5c
biiwz <biiwzmethom@gmail.com>
2022-02-10 20:03:21 +0700

    using getters

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="fixed z-10 inset-0 overflow-y-auto hidden" id="modal">
+  <div
+    class="fixed z-10 inset-0 overflow-y-auto"
+    :class="{ hidden: !authModalShow }"
+    id="modal"
+  >
     <div
       class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
     >
@@ -150,6 +154,11 @@
 <script>
 export default {
   name: 'Auth',
+  computed: {
+    authModalShow() {
+      return this.$store.state.authModalShow;
+    },
+  },
 };
 </script>
 
