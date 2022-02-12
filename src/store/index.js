75a8d923204562fef0ea266d878625f867dd4c5b
biiwz <biiwzmethom@gmail.com>
2022-02-12 14:03:27 +0700

    setting up the login

--- src/store/index.js
+++ src/store/index.js
@@ -36,6 +36,11 @@ export default createStore({
 
       ctx.commit('toggleAuth');
     },
+    async login(ctx, payload) {
+      await auth.signInWithEmailAndPassword(payload.email, payload.password);
+
+      this.commit('toggleAuth');
+    },
     init_login({ commit }) {
       const user = auth.currentUser;
 
